7b0907db0b7cbdf29ff86705aec3680ab044e1f1
sarakhaled95 <architectsarakhaled@outlook.com>
2024-04-09 00:39:33 +0000

    12

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,8 @@
+xport default function createReportObject(employeesList) {
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments: (employeesList) => Object.keys(employeesList).length,
+  };
+}
