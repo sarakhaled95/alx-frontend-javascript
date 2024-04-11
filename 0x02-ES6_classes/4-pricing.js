54dbb16322844904bae51232c1b45aeeca497126
sarakhaled95 <architectsarakhaled@outlook.com>
2024-04-11 20:38:51 +0000

    4

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,11 +1,11 @@
-import Currency from './3-currency'
+import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
     if (amount !== 'number') {
-      throw new TypeError('Amount must be a number')
+      throw new TypeError('Amount must be a number');
     } else if (!(currency instanceof Currency)) {
-      throw new TypeError('Currency must be an instance of class Currency')
+      throw new TypeError('Currency must be an instance of class Currency');
     }
 
     this._amount = amount;
@@ -22,14 +22,14 @@ export default class Pricing {
 
   set amount(amount) {
     if (amount !== 'number') {
-      throw new TypeError('Amount must be a number')
+      throw new TypeError('Amount must be a number');
     }
     this._amount = amount;
   }
 
   set currency(currency) {
     if (!(currency instanceof Currency)) {
-      throw new TypeError('Currency must be an instance of class Currency')
+      throw new TypeError('Currency must be an instance of class Currency');
     }
     this._currency = currency;
   }
