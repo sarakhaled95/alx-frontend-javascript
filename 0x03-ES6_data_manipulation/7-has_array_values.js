4393f93d16937b609cdc6f831d0264c9bee625e0
sarakhaled95 <architectsarakhaled@outlook.com>
2024-04-16 17:50:09 +0000

    7

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,3 +1,3 @@
-export default function hasValuesFromArray(set, array) {
+export default function hasValuesFromArray(set, arr) {
   return arr.reduce((prev, curr) => prev && set.has(curr), true);
 }
